35536b8800604d520f5f0ea8ad9c1c00bf921622
Amit <amit0805kumar@gmail.com>
2018-10-03 22:36:36 +0530

    BST started

---
+++ JAVA/DATA_STRUCTURE/BST/BST/src/bst/BST.java
@@ -0,0 +1,38 @@
+package bst;
+import java.util.*;
+/**
+ *
+ * @author Amit Kumar
+ */
+class Node {
+
+    Node left, right;
+    int data;
+
+    Node(int item) {
+        this.data = item;
+        left = right = null;
+    }
+}
+
+public class BST {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            
+            HashMap<Integer,Node> h = new HashMap<>();
+            int n = sc.nextInt();
+            Node root = null;
+            while(n>0){
+                
+                   
+                
+                n--;
+            }
+            t--;
+        }
+    }
+
+}
--- JSON/script.js
+++ JSON/script.js
@@ -1,6 +1,25 @@
-var x = {
+/*var x = {
     "NAME": "Amit Kumar",
-    "AGE": "19"
+    "AGE": "19",
+    "NUM": [{
+        "num1": "12"
+    }, {
+        "num2": 33
+    }]
 
 }
-document.querySelector(".cont").innerHTML =x.NAME;
+var y = { "name" : [ { "name1" : "value1" }, { "name2" : "value2" }] }
+// document.querySelector(".cont").innerHTML =x.NAME;
+
+var z=  {"name": {"name1": 1,"name2": 1}}*/
+
+var obj = {
+  fname: "Amit",
+    lname: "Kumar",
+    age:20,
+    cal: function(){
+        alert(this.age);
+    }
+};
+
+document.getElementById("this").onmouseover = obj.cal;
