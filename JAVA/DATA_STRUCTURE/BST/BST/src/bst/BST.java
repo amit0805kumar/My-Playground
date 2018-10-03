35479e023a74907424702c2795c59a2496ca197e
Amit <amit0805kumar@gmail.com>
2018-10-03 23:29:06 +0530

    BST created using HASHMAP

--- JAVA/DATA_STRUCTURE/BST/BST/src/bst/BST.java
+++ JAVA/DATA_STRUCTURE/BST/BST/src/bst/BST.java
@@ -1,5 +1,7 @@
 package bst;
+
 import java.util.*;
+
 /**
  *
  * @author Amit Kumar
@@ -20,19 +22,39 @@ public class BST {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t>0){
-            
-            HashMap<Integer,Node> h = new HashMap<>();
+        while (t > 0) {
+
+            HashMap<Integer, Node> h = new HashMap<>();
             int n = sc.nextInt();
             Node root = null;
-            while(n>0){
-                
-                   
-                
+            while (n > 0) {
+
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+
+                Node parent = h.get(n1);
+                if (parent == null) {
+                    parent = new Node(n1);
+                    h.put(n1, parent);
+                    if (root == null) {
+                        root = parent;
+                    }
+                }
+
+                Node child = new Node(n2);
+                if (lr == 'L') {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+                h.put(n2, child);
                 n--;
             }
+             System.out.println(h);
             t--;
         }
+        
     }
 
 }
